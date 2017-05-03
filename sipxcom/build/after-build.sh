6c84039266bab2e25a92b567a3f42d1f4c3446eb
Roman Galeev <roman.galeev@ezuce.com>
2017-05-03 22:12:08 +0200

    sipXconfig after-build dependencies

--- sipxcom/build/after-build.sh
+++ sipxcom/build/after-build.sh
@@ -1,2 +1,4 @@
 #!/bin/sh -e
-yum install -y cfengine
+yum install -y cfengine mod_ssl postgresql-server mongodb-server elasticsearch \
+	vsftpd ntpd xinetd net-snmp net-snmp-utils \
+	python-pymongo python-argparse
