f78be4873580aabed1fb693df96b731ea211014f
Roman Galeev <roman.galeev@ezuce.com>
2017-05-11 15:15:43 +0200

    Increase verbosity of ezuce-ci builder

--- ezuce-ci/build.sh
+++ ezuce-ci/build.sh
@@ -1,9 +1,18 @@
 #!/bin/sh -e
 TOKEN=$1
-DOCKER=${2:-"$(getent group docker | cut -d: -f3)"}
-if [ -z $TOKEN ] || [ -z $DOCKER ]
+DOCKER_GROUP=${2:-"$(getent group docker | cut -d: -f3)"}
+if [ -z $DOCKER_GROUP ]
+then
+	echo "Can't automatically deduce docker group, please provide: $0 token docker_gid"
+	exit 1
+fi
+
+if [ -z $TOKEN ]
 then
 	echo Please provide a token to access private repo: $0 token [docker_gid]
 	exit 1
 fi
-docker build $BUILD_FLAGS -t ezuce/ci --build-arg TOKEN=$TOKEN --build-arg DOCKER=$DOCKER .
+
+echo Building with docker group: $DOCKER_GROUP
+
+docker build $BUILD_FLAGS -t ezuce/ci --build-arg TOKEN=$TOKEN --build-arg DOCKER=$DOCKER_GROUP .
