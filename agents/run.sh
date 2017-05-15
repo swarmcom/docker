1fbce8e6936b5e0d88094f5ffd3e5727ad256b30
Roman Galeev <roman.galeev@ezuce.com>
2017-05-15 16:46:22 +0200

    Add agents sink for testing

---
+++ agents/run.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -e
+FLAGS=${1:-"-td"}
+IMAGE=${2:-"ezuce/agents"}
+NETWORK=${NETWORK:-"ezuce"}
+NAME=${NAME:-"agents.$NETWORK"}
+
+if [ -n "$(docker ps -aq -f name=$NAME)" ]
+then
+   echo -n "stopping: "
+   docker stop -t 1 $NAME
+   echo -n "removing: "
+   docker rm -f $NAME
+fi
+
+echo -n "starting: $NAME "
+docker run $FLAGS \
+	--net $NETWORK \
+	-h $NAME \
+	--name $NAME \
+	$IMAGE
