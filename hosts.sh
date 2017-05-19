bdcdff777d1f1e981c31d774240aec5db513f5b6
Roman Galeev <roman.galeev@ezuce.com>
2017-05-19 15:25:06 +0200

    Add Kibana container

--- hosts.sh
+++ hosts.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 NETWORK=${1:-"ezuce"}
-for CONTAINER in mongodb sipxcom redis freeswitch elastic reach agents
+for CONTAINER in mongodb sipxcom redis freeswitch elastic reach agents kibana
 do
 	if [ "$(docker inspect -f {{.State.Running}} $CONTAINER.$NETWORK)" = "true" ]
 	then
