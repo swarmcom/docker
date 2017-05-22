9cb36776836ac1cd0e7a9b0bae1476b6b1599a37
dizzy <george@ezuce.com>
2017-05-22 17:15:25 +0300

    Add travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "running on the remote machine!!!"
-sudo docker run --name mongodb ezuce/mongodb
-sudo docker run --name mongodb ezuce/redis
+sudo docker run -d --name mongodb ezuce/mongodb
+sudo docker run -d --name mongodb ezuce/redis
