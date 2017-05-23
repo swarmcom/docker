36652bc41af25b952e66ae26c34024425496cf15
Mihai <costache.mircea.mihai@gmail.com>
2017-05-23 14:09:11 +0300

    Starting fs and erlang containers

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,3 +2,5 @@
 echo "running on the remote machine!!!"
 sudo docker run -d --name mongodb ezuce/mongodb
 sudo docker run -d --name redis ezuce/redis
+sudo docker run -d --name redis ezuce/freeswitch
+sudo docker run -d --name redis ezuce/erlang
