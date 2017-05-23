94eb61fa832334c0081e989cb335a12e501904a0
Mihai <costache.mircea.mihai@gmail.com>
2017-05-23 15:45:06 +0300

    adding  openjdk-8-jdk

--- base-os/build/setup-os.sh
+++ base-os/build/setup-os.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 apt-get -y update && apt-get -y upgrade
 
-apt-get install -y make build-essential zip unzip git wget curl
+apt-get install -y make build-essential zip unzip git wget curl  openjdk-8-jdk
 apt-get -y clean
 
 useradd -s /bin/bash -m user
