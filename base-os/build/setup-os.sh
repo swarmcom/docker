ea4e0fb53647868f2e6d7a5d1ba9a1485460a7b3
Mihai <costache.mircea.mihai@gmail.com>
2017-05-23 17:01:50 +0300

    revert openjdk

--- base-os/build/setup-os.sh
+++ base-os/build/setup-os.sh
@@ -1,18 +1,6 @@
 #!/bin/sh -e
 apt-get -y update && apt-get -y upgrade
 
-# Add below lines to install openjdk
-echo 'deb http://deb.debian.org/debian jessie-backports main' \
-      > /etc/apt/sources.list.d/jessie-backports.list
- 
-apt update -y
-
-apt install --target-release jessie-backports \
-      openjdk-8-jre-headless \
-      ca-certificates-java \
-      --assume-yes
-## finish openjdk install 
-
 apt-get install -y make build-essential zip unzip git wget curl  
 apt-get -y clean
 
