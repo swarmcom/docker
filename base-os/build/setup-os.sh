703a27df90e47fd6cca6f7b348f06d331bcd60be
Mihai <costache.mircea.mihai@gmail.com>
2017-05-23 16:16:59 +0300

    installing openjdk

--- base-os/build/setup-os.sh
+++ base-os/build/setup-os.sh
@@ -1,7 +1,19 @@
 #!/bin/sh -e
 apt-get -y update && apt-get -y upgrade
 
-apt-get install -y make build-essential zip unzip git wget curl  openjdk-8-jdk
+# Add below lines to install openjdk
+echo 'deb http://deb.debian.org/debian jessie-backports main' \
+      > /etc/apt/sources.list.d/jessie-backports.list
+ 
+apt update -y
+
+apt install --target-release jessie-backports \
+      openjdk-8-jre-headless \
+      ca-certificates-java \
+      --assume-yes
+## finish openjdk install 
+
+apt-get install -y make build-essential zip unzip git wget curl  
 apt-get -y clean
 
 useradd -s /bin/bash -m user
