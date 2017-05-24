6a08d60910d25d4483ef3c662db385da7276a70b
Mihai <costache.mircea.mihai@gmail.com>
2017-05-24 09:50:06 +0300

    revert openjdk

--- erlang/build/os.sh
+++ erlang/build/os.sh
@@ -2,18 +2,7 @@
 apt-get install -y python libexpat-dev \
 	unixodbc-dev libssh2-1-dev libssl-dev libncurses5-dev
 	
-	
-# Added below lines to install openjdk
-echo 'deb http://deb.debian.org/debian jessie-backports main' \
-      > /etc/apt/sources.list.d/jessie-backports.list
- 
-apt update -y
 
-apt install --target-release jessie-backports \
-      openjdk-8-jre-headless libssl-dev \
-      ca-certificates-java \
-      --assume-yes
-## finish openjdk install 	
 if [ ! -z $WITH_XML ]
 then
 	apt-get install -y xsltproc libxml2-utils fop
