3ee9cf52c72d76b783b37b15e298eb7323520d59
Mihai <costache.mircea.mihai@gmail.com>
2017-05-24 09:04:17 +0300

    revert openjdk install attempt from here

--- erlang/build/erlang.sh
+++ erlang/build/erlang.sh
@@ -1,17 +1,7 @@
 #!/bin/sh -e
 cd ~
 
-# Added below lines to install openjdk
-echo 'deb http://deb.debian.org/debian jessie-backports main' \
-      > /etc/apt/sources.list.d/jessie-backports.list
- 
-apt update -y
 
-apt install --target-release jessie-backports \
-      openjdk-8-jre-headless libssl-dev \
-      ca-certificates-java \
-      --assume-yes
-## finish openjdk install 
 
 
 curl -O -k https://raw.githubusercontent.com/kerl/kerl/master/kerl
