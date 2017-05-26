8bd88e827d6973e776f1df73b580d8d1e5bead11
dizzy <george@ezuce.com>
2017-05-26 13:56:08 +0300

    Build FS from external sources

--- freeswitch/build.sh
+++ freeswitch/build.sh
@@ -1,8 +1,16 @@
 #!/bin/sh -e
 NETWORK=${NETWORK:-"ezuce"}
-REPO=${2:-"https://freeswitch.org/stash/scm/fs/freeswitch.git"}
+REPO=${2:-"https://github.com/sipXcom/freeswitch.git"}
 COMMIT=${1:-"$(../bin/get-commit $REPO)"}
 
 echo $COMMIT > etc/commit
+cd src
+if [ ! -d freeswitch ]; then
+  git clone $REPO freeswitch
+  cd freeswitch
+  git checkout release-17.08
+  cd ../
+fi
+cd ../
 
 docker build $BUILD_FLAGS --build-arg REPO=$REPO -t $NETWORK/freeswitch .
