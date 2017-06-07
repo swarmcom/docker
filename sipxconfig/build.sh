8e37134e88564f4687fe767ca6cea6647f042eb2
dizzy <george@ezuce.com>
2017-06-07 08:54:52 +0300

    Checkout docker-sipxconfig branch

--- sipxconfig/build.sh
+++ sipxconfig/build.sh
@@ -2,7 +2,7 @@
 cd src
 if [ ! -d sipxecs ]; then
   git clone --depth 1 git://github.com/sipXcom/sipxecs.git
-  cd sipxecs && git checkout release-17.08
+  cd sipxecs && git checkout docker-sipxconfig
   cd ../
 fi
 cd ../
