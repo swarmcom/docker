2b94ae7d90f1712f5b156e31047a19b7d8b194df
Mihai <costache.mircea.mihai@gmail.com>
2017-06-11 08:42:16 +0300

    doiyng complete clone without depth

--- sipxconfig/build.sh
+++ sipxconfig/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -e
 cd src
 if [ ! -d sipxecs ]; then
-  git clone --depth 1 https://github.com/sipXcom/sipxecs.git
-  cd sipxecs && git checkout -b branch docker-sipxconfig
+  git clone  https://github.com/sipXcom/sipxecs.git
+  cd sipxecs && git checkout docker-sipxconfig
   cd ../
 fi
 cd ../
