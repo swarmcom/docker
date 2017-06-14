fddb164d58b57dc78941f5f2c060261513399a47
Mihai-CMM <costache.mircea.mihai@gmail.com>
2017-06-14 13:32:59 +0300

    proxy add from siplibs

---
+++ siplibs/build/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh -e
+export SIPXPBXGROUP=user
+export SIPXPBXUSER=user
+cd ~/build/
+make sipXportLib && make sipXtackLib && make sipXmediaLib && make sipXmediaAdapterLib && make sipXcallLib && make sipXcommserverLib &&  make sipXcommons
+rm -rf ~/sipxecs
