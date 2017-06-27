64df567f3520610740fe816688255121e83b55e4
Roman Galeev <roman.galeev@ezuce.com>
2017-06-27 14:58:49 +0200

    Fix run.sh paths

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ NETWORK=${1:-"ezuce"}
 export NETWORK
 echo -n "starting network: $NETWORK "
 docker network create $NETWORK
-cd ../mongodb && ./run.sh
+cd ./mongodb && ./run.sh
 cd ../elastic && ./run.sh
 cd ../kibana && ./run.sh
 cd ../freeswitch && ./run.sh
@@ -11,4 +11,4 @@ cd ../agents && ./run.sh
 cd ../reach && ./run.sh
 
 echo Add these to your /etc/hosts
-./hosts.sh
+cd ../ && ./hosts.sh
