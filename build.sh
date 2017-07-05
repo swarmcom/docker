d53b220a6996a7d85c6a437c60b531ac63308a3a
Mihai-CMM <costache.mircea.mihai@gmail.com>
2017-07-05 09:00:45 +0300

    Build and publish siplibs due to running out of disk on travis-ci

--- build.sh
+++ build.sh
@@ -11,6 +11,11 @@ fi
 # cd postgres-sipxconfig && ./build.sh
 #cd ../sipxconfig && ./build.sh
 
+#We will build and publish siplibs first due to high disk usage demanded by our build that's breaking travis-ci
 cd siplibs && ./build.sh
-cd ../sipxproxy && ./build.sh
-cd ../sipxregistrar && ./build.sh
+
+#cd ../sipxproxy && ./build.sh
+#cd ../sipxregistrar && ./build.sh
+
+
+#Will then need to modify both travis.yml and this file to build and publish proxy and registrar
