88b1653280fa668035bf1ddb91ec07837a1b4fe2
Roman Galeev <roman.galeev@ezuce.com>
2017-07-11 16:57:36 +0200

    Add missing dependencies

--- reach-centos6/build/setup-os.sh
+++ reach-centos6/build/setup-os.sh
@@ -3,7 +3,7 @@ adduser -d /home/user user
 yum update -y
 
 # erlang build-deps (except for xml docs)
-yum install -y wget curl expat-devel unixODBC-devel libssh2-devel openssl-devel ncurses-devel gcc-c++ gcc make
+yum install -y wget curl expat-devel unixODBC-devel libssh2-devel openssl-devel ncurses-devel gcc-c++ gcc make tar which
 
 # centos 6 git doesn't understand TOKEN
 yum install -y http://opensource.wandisco.com/centos/6/git/x86_64/wandisco-git-release-6-1.noarch.rpm
