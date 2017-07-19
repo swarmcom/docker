7f9af2544bd0e416001a8c28de69099ec95fd239
Roman Galeev <roman.galeev@ezuce.com>
2017-07-19 13:05:11 +0200

    Avoid name clash with HOSTNAME

--- busytone/run.sh
+++ busytone/run.sh
@@ -2,23 +2,23 @@
 FLAGS=${FLAGS:-"-td"}
 NETWORK=${NETWORK:-"ezuce"}
 AGENTS_NODE=${AGENTS_NODE:-"agents@agents.$NETWORK"}
-HOSTNAME=${HOSTNAME:-"busytone.$NETWORK"}
+NAME=${NAME:-"busytone.$NETWORK"}
 REACH_HOST=${REACH_HOST:-"reach.$NETWORK"}
-NODE=${NODE:-"busytone@$HOSTNAME"}
+NODE=${NODE:-"busytone@$NAME"}
 
-if [ -n "$(docker ps -aq -f name=$HOSTNAME)" ]
+if [ -n "$(docker ps -aq -f name=$NAME)" ]
 then
 	echo -n "stopping: "
-	docker stop -t 1 $HOSTNAME
+	docker stop -t 1 $NAME
 	echo -n "removing: "
-	docker rm -f $HOSTNAME
+	docker rm -f $NAME
 fi
 
-echo -n "starting: $HOSTNAME "
+echo -n "starting: $NAME "
 docker run $FLAGS \
 	--net $NETWORK \
-	-h $HOSTNAME \
-	--name $HOSTNAME \
+	-h $NAME \
+	--name $NAME \
 	--env NETWORK=$NETWORK \
 	--env NODE=$NODE \
 	--env AGENTS_NODE=$AGENTS_NODE \
--- reach/run.sh
+++ reach/run.sh
@@ -4,23 +4,23 @@ NETWORK=${NETWORK:-"ezuce"}
 MONGODB=${MONGODB:-"mongodb.$NETWORK"}
 ELASTIC=${ELASTIC:-"elastic.$NETWORK"}
 SIPXCOM=${SIPXCOM:-"sipxcom.$NETWORK"}
-HOSTNAME=${HOSTNAME:-"reach.$NETWORK"}
+NAME=${NAME:-"reach.$NETWORK"}
 FSNODE=${FSNODE:-"freeswitch@freeswitch.$NETWORK"}
-NODE=${NODE:-"reach@$HOSTNAME"}
+NODE=${NODE:-"reach@$NAME"}
 
-if [ -n "$(docker ps -aq -f name=$HOSTNAME)" ]
+if [ -n "$(docker ps -aq -f name=$NAME)" ]
 then
 	echo -n "stopping: "
-	docker stop -t 1 $HOSTNAME
+	docker stop -t 1 $NAME
 	echo -n "removing: "
-	docker rm -f $HOSTNAME
+	docker rm -f $NAME
 fi
 
-echo -n "starting: $HOSTNAME "
+echo -n "starting: $NAME "
 docker run $FLAGS \
 	--net $NETWORK \
-	-h $HOSTNAME \
-	--name $HOSTNAME \
+	-h $NAME \
+	--name $NAME \
 	--env NETWORK=$NETWORK \
 	--env MONGODB=$MONGODB \
 	--env ELASTIC=$ELASTIC \
