e78d11aa2171f2f3e2f7c4dc5452281f4cdf3c51
Roman Galeev <roman.galeev@ezuce.com>
2017-08-28 22:09:04 +0200

    Use complete URI

--- reach-ui/run.sh
+++ reach-ui/run.sh
@@ -2,7 +2,7 @@
 FLAGS=${FLAGS:-"-td"}
 NETWORK=${NETWORK:-"ezuce"}
 NAME=${NAME:-"reach-ui.$NETWORK"}
-REACH=${REACH:-"reach.$NETWORK"}
+REACH_WS=${REACH_WS:-"ws://reach.$NETWORK:8937/ws"}
 
 if [ -n "$(docker ps -aq -f name=$NAME)" ]
 then
@@ -18,5 +18,5 @@ docker run $FLAGS \
 	-h $NAME \
 	--name $NAME \
 	--env NETWORK=$NETWORK \
-	--env REACH_WS="ws://$REACH/ws" \
+	--env REACH_WS=$REACH_WS \
 	$NETWORK/reach-ui
