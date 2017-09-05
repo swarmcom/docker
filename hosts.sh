aaaca296a4f1ff61f175280d3146d1f07bec4cb8
Roman Galeev <roman.galeev@ezuce.com>
2017-09-05 13:44:54 +0200

    Add rr container to hosts.sh to auto-generate

--- hosts.sh
+++ hosts.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 NETWORK=${1:-"ezuce"}
-for CONTAINER in mongodb freeswitch elastic reach kibana agents busytone reach-ui
+for CONTAINER in mongodb freeswitch elastic reach kibana agents busytone reach-ui rr
 do
 	if [ "$(docker inspect -f {{.State.Running}} $CONTAINER.$NETWORK)" = "true" ]
 	then
