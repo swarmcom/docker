f2dc9e8d17afa65cd0b576e1160345f74a070454
Roman Galeev <roman.galeev@ezuce.com>
2017-09-14 12:59:31 +0200

    Use common mechanics to pass arguments in env vars

--- freeswitch-reach3/build.sh
+++ freeswitch-reach3/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 NETWORK=${NETWORK:-"ezuce"}
-REPO=${2:-"https://freeswitch.org/stash/scm/fs/freeswitch.git"}
-COMMIT=${1:-"$(../bin/get-commit $REPO)"}
+REPO=${REPO:-"https://freeswitch.org/stash/scm/fs/freeswitch.git"}
+COMMIT=${COMMIT:-"$(../bin/get-commit $REPO)"}
 
 echo $COMMIT > etc/commit
 
