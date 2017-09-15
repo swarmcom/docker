64f6195d7ee39c42079d9a009a8c734c04342e91
senescu <stefanc.enescugmail.com>
2017-09-15 19:43:23 +0300

    Fix FS sounds in install.sh

--- freeswitch-1708/build/install.sh
+++ freeswitch-1708/build/install.sh
@@ -4,6 +4,7 @@ TARGET=/usr/local/freeswitch
 
 cd freeswitch
 make install
+make sounds-install
 
 groupadd freeswitch
 adduser --disabled-password  --quiet --system --home $TARGET --ingroup freeswitch freeswitch
@@ -11,9 +12,14 @@ adduser --disabled-password  --quiet --system --home $TARGET --ingroup freeswitc
 rm -rf $TARGET/conf
 cp -a ../build/conf $TARGET/conf
 
+
 rm -rf $TARGET/sounds
-mv ../build/sounds $TARGET/sounds
+mkdir sounds
+tar -zxvf freeswitch-sounds-en-us-callie-8000-1.0.51.tar.gz -C sounds
+mv sounds $TARGET/sounds
+
 
 chown -R freeswitch:freeswitch $TARGET
 chmod -R ug=rwX,o= $TARGET
 chmod -R u=rwx,g=rx $TARGET/bin/*
+
