d91e828d8a9669ca644fed3c15111b4d4ff20828
mirceac <mircea.carasel@gmail.com>
2017-09-21 09:38:54 +0300

    SIPX-689: Integrate conference service with freeswitch

--- sipxconfig/build/build.sh
+++ sipxconfig/build/build.sh
@@ -2,5 +2,5 @@
 export SIPXPBXGROUP=user
 export SIPXPBXUSER=user
 cd ~/build/
-make sipXcommserverLib && make sipXportLib && make sipXsupervisor && make sipXcommons && make sipXcdr && make sipXconfig && make sipXcom
+make sipXcommserverLib && make sipXportLib && make sipXsupervisor && make sipXcommons && make sipXcdr && make sipXconfig && make sipXcom && make sipXprompts
 rm -rf ~/sipxecs
--- supervisor/watcher.sh
+++ supervisor/watcher.sh
@@ -3,7 +3,7 @@
 portRegistrar="-p 5070:5070 -p 5070:5070/udp"
 portProxy="-p 5060:5060 -p 5060:5060/udp"
 portCdr="-p 8130:8130 -p 8130:8130/udp"
-portFreeswitch="-p 15060:15060 -p 15060:15060/udp -p 8084:8084 -p 8184:8184 -p 8284:8284 -p 11000-11010:11000-11010/udp -p 11000-11010:11000-11010/tcp"
+portFreeswitch="-p 15060:15060 -p 15060:15060/udp -p 8084:8084 -p 8184:8184 -p 8284:8284 --expose=11000-12999/udp"
 
 watch -n 2 "/usr/bin/file-watcher.sh; /usr/bin/process-watcher.sh sipxregistrar '$portRegistrar' ezuce-private; /usr/bin/process-watcher.sh sipxproxy '$portProxy' ezuce-public; /usr/bin/process-watcher.sh sipxcdr '$portCdr' ezuce-private; /usr/bin/process-watcher.sh sipxfreeswitch  '$portFreeswitch' ezuce-private"
 
