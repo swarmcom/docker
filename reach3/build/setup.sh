3b770d59308ccc5136e8b6bc546bf8f0c93d60e3
Roman Galeev <roman.galeev@ezuce.com>
2017-10-11 13:14:24 +0200

    Provide name and email for git

--- reach3/build/setup.sh
+++ reach3/build/setup.sh
@@ -3,6 +3,10 @@
 
 git clone $REPO reach && cd reach
 
+# avoid git complains
+git config --global user.email "roman.galeev@ezuce.com"
+git config --global user.name "Roman Galeev"
+
 git fetch origin $BRANCH:build_branch
 git checkout build_branch
 git clean -d -f
