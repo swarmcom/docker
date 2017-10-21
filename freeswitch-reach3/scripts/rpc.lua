bb9b2ebdb1b6dec328eab8f1bb2e55455ffb033c
Roman Galeev <roman.galeev@ezuce.com>
2017-10-21 22:00:11 +0200

    Better Lua rpc script (use env, and err printer)

--- freeswitch-reach3/scripts/rpc.lua
+++ freeswitch-reach3/scripts/rpc.lua
@@ -1,16 +1,21 @@
 -- retrieve the content of a URL
-local uri = argv[1]
+function fs_err(...)
+	freeswitch.consoleLog("err", "LuaRPC " .. string.format(unpack{...}) .. "\n")
+end
+
+local lua_id = argv[1]
+local uri = string.format("%s/lua/%s", os.getenv("REACH_HOST"), lua_id)
 
 local http = require("socket.http")
 local body, code = http.request(uri)
 if code ~= 200 then
-	freeswitch.consoleLog("err", "Lua rpc http error: " .. code .. "\n");
+	fs_err("http lua script uri:%s error:%s", uri, code)
 	return code
 end
 
 local script, error = loadstring(body)
 if not script then
-	freeswitch.consoleLog("err", "Lua rpc compile error: " .. error .. "\n");
+	fs_err("lua script syntax error: %s", error)
 	return error
 end
 
