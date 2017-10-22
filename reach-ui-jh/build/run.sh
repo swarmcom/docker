687ad63ef61cd65b01612eef50d5afb8087f1f46
Roman Galeev <roman.galeev@ezuce.com>
2017-10-22 19:06:34 +0200

    Add commas

--- reach-ui-jh/build/run.sh
+++ reach-ui-jh/build/run.sh
@@ -2,14 +2,14 @@
 cd reach-ui
 if [ -n $REACH_WS ]
 then
-	sed -i "s|reach_ws:.*|reach_ws:\ \"$REACH_WS\"|" dist/config.js
+	sed -i "s|reach_ws:.*|reach_ws:\ \"$REACH_WS\",|" dist/config.js
 else
-	sed -i "s|reach_ws:.*|reach_ws:\ undefined|" dist/config.js
+	sed -i "s|reach_ws:.*|reach_ws:\ undefined,|" dist/config.js
 fi
 if [ -n $REACH_HTTP ]
 then
-	sed -i "s|reach_ws:.*|reach_http:\ \"$REACH_HTTP\"|" dist/config.js
+	sed -i "s|reach_ws:.*|reach_http:\ \"$REACH_HTTP\",|" dist/config.js
 else
-	sed -i "s|reach_ws:.*|reach_http:\ undefined|" dist/config.js
+	sed -i "s|reach_ws:.*|reach_http:\ undefined,|" dist/config.js
 fi
 node_modules/.bin/http-server ./
