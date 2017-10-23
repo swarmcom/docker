0fe3e6a509cf14b21a306cca959bc6d620b93b85
Roman Galeev <roman.galeev@ezuce.com>
2017-10-24 00:03:50 +0200

    Add keep-alive?

--- reach-centos6/build/setup-erlang.sh
+++ reach-centos6/build/setup-erlang.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -e
 curl -O https://raw.githubusercontent.com/kerl/kerl/master/kerl
 chmod +x kerl
+while true; do echo "I'm alive" && sleep 1; done &
 ./kerl build 19.3 19.3
 ./kerl install 19.3 erlang/
 . erlang/activate
