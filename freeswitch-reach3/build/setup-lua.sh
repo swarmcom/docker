936425bb98bfa737acd54f2e41de9f93ab047c7c
Roman Galeev <roman.galeev@ezuce.com>
2017-11-07 12:38:48 +0100

    Rely on socket library for HTTP RPC

--- freeswitch-reach3/build/setup-lua.sh
+++ freeswitch-reach3/build/setup-lua.sh
@@ -1,9 +1,1 @@
 #!/bin/sh -e
-wget http://luarocks.github.io/luarocks/releases/luarocks-2.4.3.tar.gz
-tar zxvf luarocks-2.4.3.tar.gz
-cd luarocks-2.4.3
-./configure
-make build
-make install
-luarocks install http
-ln -s /usr/local/share/lua/5.2/http_0_2_0-http /usr/local/share/lua/5.2/http
--- freeswitch-reach3/scripts/rpc.lua
+++ freeswitch-reach3/scripts/rpc.lua
@@ -1,12 +1,19 @@
+-- call format: rpc.lua lua_id lua_token args...
+
+local http = require("socket.http")
+local json = require("json")
+
 -- retrieve the content of a URL
 function fs_err(...)
 	freeswitch.consoleLog("err", "LuaRPC " .. string.format(unpack{...}) .. "\n")
 end
 
 local lua_id = table.remove(argv, 1)
-local uri = string.format("%s/lua/%s", os.getenv("REACH_HOST"), lua_id)
+local lua_token = table.remove(argv, 1)
 
-local http = require("socket.http")
+local uri = string.format("%s/lua/%s", os.getenv("REACH_HOST"), lua_id)
+local lua_rpc_uri = string.format("%s/lua/rpc", os.getenv("REACH_HOST"))
+
 local body, code = http.request(uri)
 if code ~= 200 then
 	fs_err("http lua script uri:%s error:%s", uri, code)
@@ -17,6 +24,28 @@ local script, error = loadstring(body)
 if not script then
 	fs_err("lua script syntax error: %s", error)
 	return error
+end
+
+function rpc(F, ...)
+	local request = {
+		auth = lua_token,
+		type = "call",
+		args = {"rpc_lua", F, {...}}
+	}
+	local encoded_request = json.encode(request)
+	local body = {}
+	local re, code = http.request{
+		url = lua_rpc_uri,
+		method = "POST",
+		headers = {
+			["content-type"] = "application/json",
+			["content-length"] = tostring(#encoded_request)
+		},
+		source = ltn12.source.string(encoded_request),
+		sink = ltn12.sink.table(body)
+	}
+	local response = json.decode(table.concat(body))
+	return response.reply
 end
 
 script(unpack(argv))
