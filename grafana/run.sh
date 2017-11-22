71279c39aa97e2066977177718968694c8e71d5c
Roman Galeev <roman.galeev@ezuce.com>
2017-11-23 00:35:40 +0100

    Configure grafana root for docker env

--- grafana/run.sh
+++ grafana/run.sh
@@ -2,6 +2,7 @@
 FLAGS=${FLAGS:-"-td"}
 NETWORK=${NETWORK:-"ezuce"}
 NAME=${NAME:-"grafana.$NETWORK"}
+GF_SERVER_ROOT_URL=https://docker.ezuce.com/grafana
 
 if [ -n "$(docker ps -aq -f name=$NAME)" ]
 then
@@ -17,4 +18,5 @@ docker run $FLAGS \
 	-h $NAME \
 	--name $NAME \
 	--env NETWORK=$NETWORK \
+	--env GF_SERVER_ROOT_URL=$GF_SERVER_ROOT_URL \
 	$NETWORK/grafana
