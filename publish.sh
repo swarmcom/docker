a28faae6e825dc449fa7c85ad69a48d8b1d8af44
Roman Galeev <roman.galeev@ezuce.com>
2018-01-29 21:59:07 +0100

    Add publish script

---
+++ publish.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+docker push ezuce/reach
+docker push ezuce/reach-ui
+docker push ezuce/reach-ui-jh
+docker push ezuce/nginx-ingress
+docker push ezuce/kamailio
+docker push ezuce/freeswitch-reach3
+docker push ezuce/rr
+docker push ezuce/ci
+docker push ezuce/timescale
+docker push ezuce/agents
+docker push ezuce/busytone
+
