0086bc1b3b3243a81ec09e7ba5cdecdc71f46a6d
Roman Galeev <roman.galeev@ezuce.com>
2018-01-29 21:56:10 +0100

    Fix topmost build/run

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for FOLDER in base-os erlang freeswitch-reach3 freeswitch-agents timescale reach3 rr reach-ui busytone freeswitch-agents
+for FOLDER in base-os erlang freeswitch-reach3 freeswitch-agents timescale reach3 rr reach-ui busytone
 do
 	cd $FOLDER && ./build.sh && cd ../
 done
