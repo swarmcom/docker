d163898bee000229561e6d3435cf6df73e5de052
Roman Galeev <roman.galeev@ezuce.com>
2018-02-14 12:12:44 +0000

    Force internal_ip to authenticate node

--- kamailio/build/run.sh
+++ kamailio/build/run.sh
@@ -7,10 +7,12 @@ export GROUP=user
 /home/user/erlang/bin/epmd -daemon
 
 CFG=etc/kamailio.cfg
+INT_IP=`/sbin/ifconfig eth1 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}'`
 
 sed -i "s|reach@reach.ezuce|$REACH_NODE|g" $CFG
 sed -i "s|kamailio.ezuce|$NAME|g" $CFG
 sed -i "s|external_ip|$EXT_IP|g" $CFG
+sed -i "s|internal_ip|$INT_IP|g" $CFG
 
 echo listen=udp:eth0:5060 advertise $EXT_IP:5060 > etc/kamailio-local.cfg
 echo listen=udp:eth1:5080 >> etc/kamailio-local.cfg
