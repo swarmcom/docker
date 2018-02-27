2fb4b07586521c8945d47ab1c4416cab1720e710
Roman Galeev <roman.galeev@ezuce.com>
2018-02-27 13:45:18 +0100

    Use the same network to setup pg connection

--- reach3/build/run.sh
+++ reach3/build/run.sh
@@ -3,6 +3,6 @@ COMMAND=${1:-"console"}
 . erlang/activate
 cd reach
 CFG_ROOT=config
-SYS=$CFG_ROOT/sys.config
 sed -i "s|reach@172.17.0.1|$NODE|g" $CFG_ROOT/vm.args
+sed -i "s|timescale.ezuce|timescale.$NETWORK|g" $CFG_ROOT/sys.config
 exec _build/default/rel/reach/bin/reach $COMMAND $*
