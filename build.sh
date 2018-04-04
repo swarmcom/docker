3dda11315ca7c6e1739e212ee6e93e83c90135ce
Roman Galeev <jamhedd@gmail.com>
2018-04-04 10:05:25 +0000

    Add kamailio to top-most build/run

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for FOLDER in base-os erlang freeswitch-reach3 freeswitch-agents timescale reach3 rr reach-ui busytone
+for FOLDER in base-os erlang freeswitch-reach3 freeswitch-agents timescale rr kamailio reach3 reach-ui busytone
 do
 	cd $FOLDER && ./build.sh && cd ../
 done
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for FOLDER in freeswitch-reach3 freeswitch-agents timescale reach3 rr reach-ui busytone
+for FOLDER in freeswitch-reach3 freeswitch-agents timescale reach3 rr reach-ui busytone kamailio
 do
 	cd $FOLDER && ./run.sh && cd ../
 done
