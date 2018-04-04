7e180cb90fe71929a675aa63c2a3bebbd740e913
Roman Galeev <roman.galeev@ezuce.com>
2018-04-04 09:12:31 +0000

    Support prod/devel images

--- export/reach3/extract.sh
+++ export/reach3/extract.sh
@@ -8,5 +8,7 @@ docker exec -it $CONTAINER /bin/bash -ic "cd reach && rm -rf _build/prod && make
 docker exec --user root $CONTAINER /root/find-debian-deps.sh /home/user/reach/_build/prod/rel/reach | sed -e 's/\x0D//' > etc/deps
 docker exec --user root $CONTAINER rm -f /root/find-debian-deps.sh
 docker cp -L $CONTAINER:/home/user/reach/_build/prod/rel/reach reach3
-cd reach3 && tar cf ../reach3.tar . --owner=1000 --group=1000
+cd reach3
+find ./ -name src -type d | xargs rm -r
+tar cf ../reach3.tar . --owner=1000 --group=1000
 cd ../ && rm -rf reach3
