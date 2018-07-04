c178e6b20fe7e615701cf046dae0b7d72cb19eee
Pavel Farkas <pavel.farkas@ezuce.com>
2018-07-04 14:24:16 +0200

    export kamailio
    various fixes: do not use preconfigured kamailio.cfg from dev container, copy fresh one

--- export/kamailio/extract.sh
+++ export/kamailio/extract.sh
@@ -8,7 +8,6 @@ docker cp build/find-debian-deps.sh $CONTAINER:/root
 docker exec --user root $CONTAINER /root/find-debian-deps.sh /home/user/kamailio | sed -e 's/\x0D//' > etc/deps
 docker exec --user root $CONTAINER rm -f /root/find-debian-deps.sh
 docker cp $CONTAINER:/home/user/kamailio kamailio/
-docker cp $CONTAINER:/home/user/etc kamailio/
 cp build/run.sh kamailio/
 cd kamailio && tar cf ../kamailio.tar ./ --owner=1000 --group=1000 && cd ../
 rm -rf kamailio
